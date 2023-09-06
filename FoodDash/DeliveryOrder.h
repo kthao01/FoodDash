365ccd92c84c535f92f7fc8cbd57c71fb49e6f0b
kthao01 <142838427+kthao01@users.noreply.github.com>
2023-09-06 14:03:58 -0700

    Update DeliveryOrder.h

--- FoodDash/DeliveryOrder.h
+++ FoodDash/DeliveryOrder.h
@@ -4,9 +4,22 @@
 
 #ifndef FOODDASH_DELIVERYORDER_H
 #define FOODDASH_DELIVERYORDER_H
+#include "Order.h"
+#include <string>
+
+using namespace std;
 
 
-class DeliveryOrder {
+class DeliveryOrder : public Order  {
+private:
+    string adress;
+public:
+    DeliveryOrder(string adress)
+    DeliveryOrder(string itemName, float itemPrice, string address);
+    virtual bool getDeliverStatus();
+    virtual void completeOrder();
+
+
 
 };
 
